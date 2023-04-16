252d251805474e26766e5a3a0d1fa2a176ff9996
OlekUkraine <olekfromukraine@gmail.com>
2023-04-16 22:28:50 +0300

    homework 3 ts

--- src/components/carForm/CarForm.tsx
+++ src/components/carForm/CarForm.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
 
 const CarForm = () => {
+    const {register, handleSubmit, reset, formState:{errors, isValid}} = useForm();
+
+    const save = () => {
+
+    }
+
     return (
-        <div>
-            
-        </div>
+        <form onSubmit={handleSubmit(save)}>
+            <input placeholder={''} {...register('')}/>
+            <input placeholder={''} {...register('')}/>
+            <input placeholder={''} {...register('')}/>
+
+            <button>save</button>
+        </form>
     );
 };
 
